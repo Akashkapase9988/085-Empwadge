da266754490b6b6ec582c9278ebe0818cb8d32bf
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-17 12:58:38 +0530

    [akash added program for daily empwadge]

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -7,17 +7,25 @@ namespace _085_Empwadge
         static void Main()
         {
             int ispresent = 1;
+            int empwadge = 0;
+            int rateperHr = 20;
+            int empHr = 0;
             
             Random random = new Random();
             int input = random.Next(0,2);
             if (input == ispresent)
             {
+                int emphr = 8;
                 Console.WriteLine("Employee is present");
+               
             }
             else
             {
+                int emphr = 0;
                 Console.WriteLine(" employee is absent");
             }
+            empwadge = rateperHr * empHr;
+            Console.WriteLine(empwadge);
         }
     }
 }
