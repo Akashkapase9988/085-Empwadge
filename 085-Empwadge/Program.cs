fd26da2dac71becbd960e269b7cbd489023c0d0c
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-17 13:29:26 +0530

    [akash added new program for daily wadge]

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -6,18 +6,33 @@ namespace _085_Empwadge
     {
         static void Main()
         {
+            int isfulltime = 1;
+            int isparttime = 2;
             int ispresent = 1;
+            int empwadge = 0;
+            int rateperHr = 20;
+            int empHr = 0;
             
             Random random = new Random();
-            int input = random.Next(0,2);
-            if (input == ispresent)
+            int input = random.Next(0,3);
+            if (input == isfulltime)
             {
-                Console.WriteLine("Employee is present");
+                int emphr = 8;
+                Console.WriteLine("Employee is present for fulltime");
+
+            }
+            else if (input == isparttime)
+            {
+                empHr = 4;
+                Console.WriteLine("Employee is present for Parttime");
             }
             else
             {
+                int emphr = 0;
                 Console.WriteLine(" employee is absent");
             }
+            int empwage = empHr * rateperHr;
+            Console.WriteLine("emp wadge per day is " + empwadge);
         }
     }
 }
