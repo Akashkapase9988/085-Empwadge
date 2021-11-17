7b8e51d9f2d2ffb316041831ea7f17fa7477ba74
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-17 12:36:41 +0530

    Add project files.

---
+++ 085-Empwadge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _085_Empwadge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ispresent = 1;
+            
+            Random random = new Random();
+            int input = random.Next(0,2);
+            if (input == ispresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine(" employee is absent");
+            }
+        }
+    }
+}
