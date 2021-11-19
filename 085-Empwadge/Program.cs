2347430230173582eb4293934475cc4d9c70e96e
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-19 11:29:58 +0530

    welcome to  emp wage

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -6,17 +6,7 @@ namespace _085_Empwadge
     {
         static void Main()
         {
-            int ispresent = 1;
-            
-            Random random = new Random();
-            int input = random.Next(0,2);
-            if (input == ispresent)
-            {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine(" employee is absent");
+           Console.WriteLine(" welcome to Employee wage program");
             }
         }
     }
