93def46c91c40f14c65946db5ab87485e26b6d97
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 12:02:25 +0530

    [akash added program for empwage for month1]

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -4,11 +4,44 @@ namespace _085_Empwadge
 {
     class Program
     {
-       
+        
+        /// <summary>
+        /// use of switch statement
+        /// </summary>
         public static void Main()
         {
-            Console.WriteLine("welcome to empwage program")
-           
+            int isfulltime = 1;
+            int isparttime = 2;
+            int dayinmonth = 20;
+            int empwadge = 0;
+            int rateperHr = 20;
+
+            int empHr = 0;
+            Random random = new Random();
+            int input = random.Next(0, 3);
+            switch (input)
+            {
+                case 1:
+                     empHr = 8;
+                    Console.WriteLine("Employee is present for fulltime");
+                    break;
+
+                case 2:
+                     empHr = 4;
+                    Console.WriteLine("Employee is present for Parttime");
+                    break;
+
+                default:
+                     empHr = 0;
+                    Console.WriteLine(" employee is absent");
+                    break;
+
+
+                    
+        }
+            int empwageperday = empHr * rateperHr;
+            int empwagepermonth = empwageperday * dayinmonth;
+            Console.WriteLine("emp wadge per day is " + empwagepermonth);
         }
     }
 }
