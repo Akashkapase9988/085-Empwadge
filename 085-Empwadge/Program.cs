36b2d7aacbdf3e926259325ea8483e085fd37e6f
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 12:16:14 +0530

    [akash added  progarm for max hour and max day condition]

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -2,13 +2,44 @@
 
 namespace _085_Empwadge
 {
-    class Program
+    
+    class progarm
     {
-       
-        public static void Main()
+        public const int isfulltime = 1;
+        public const int isparttime = 2;
+        public const int rateperhr = 20;
+        public const int daysinmoth = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public static void main()
         {
-            Console.WriteLine("welcome to empwage program")
-           
+            //
+            //variables
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isfulltime:
+                        empHrs = 4;
+                        break;
+                    case isparttime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#: " + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * rateperhr;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
         }
     }
 }
