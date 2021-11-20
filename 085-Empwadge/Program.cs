32eff62364246b84559900675e6bcb6dde3161ab
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-20 12:26:29 +0530

    [akash added program for attendance of employee]

--- 085-Empwadge/Program.cs
+++ 085-Empwadge/Program.cs
@@ -7,8 +7,15 @@ namespace _085_Empwadge
        
         public static void Main()
         {
-            Console.WriteLine("welcome to empwage program")
-           
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+
         }
     }
 }
